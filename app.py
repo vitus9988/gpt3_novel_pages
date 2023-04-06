9af5425cd1bed8afef8a4652cb3ee09ee6d4e734
vitus9988 <vitus9988@gmail.com>
2023-04-06 15:15:59 +0900

    lit update

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
-from flask import Flask, render_template, request
-import joblib
-import pandas as pd
+from flask import Flask, render_template, request, redirect, url_for
+
 
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
+@app.route('/paramset-page', methods=['GET','POST'])
+def paramset():
+    if request.method == "POST":
+        value = request.form['paramsform']
+        value = str(value)
+        print(value)
+    return render_template('paramset.html')
 
+
+@app.route('/novel-page', methods=['GET','POST'])
+def novel():
+    
+    return render_template('novel.html')
 
 
 
